ec5cea68760ab3db63a6e505044c03c410e6065c
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-15 00:21:18 +0530

    badi maa

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL:"https://chat-app-vyqv.onrender.com",
+  baseURL:"https://chat-app-vyqv.onrender.com/api",
   withCredentials:true,
 })
 
