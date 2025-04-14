cf24fb571e51126106ec212ea8e26bffd35dc919
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-15 02:01:12 +0530

    badi maa

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import io, { type Socket } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development"
+  import.meta.env.VITE_WORKING_MODE === "development"
     ? "http://localhost:5001" 
     : "https://chat-app-vyqv.onrender.com"; 
 
